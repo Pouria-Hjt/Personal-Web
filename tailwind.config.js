4a723df6189ac43896562d68565517105c4c601b
pouria-hjt <poriw.hjt@gmail.com>
2024-02-14 12:31:43 +0330

    add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0DB760",
+        secondary: "#0B0C0C"
+      }
+    },
   },
   plugins: [],
 }
