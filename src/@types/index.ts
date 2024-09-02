a8e476b3e09a9c8df2ac6ba9b68b97fdc36abd03
pouria-hjt <poriw.hjt@gmail.com>
2024-09-02 16:59:45 +0100

    feat(aos): add its component

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -3,3 +3,4 @@
  * @summary Common types for the project
  * @description This file exports all the common types in the project
  */
+export type { Animations } from "./Aos"
---
+++ src/ui/Animate/Animate.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React from "react";
+import { Animations } from "@/@types";
+import { easingOptions } from "aos";
+
+interface Props {
+  as: React.ElementType;
+  animation: Animations;
+  easingFunction?: easingOptions;
+  duration?: number;
+  delay?: number;
+  children?: React.ReactNode;
+}
+const Animate: React.FC<Props & React.HTMLAttributes<HTMLElement>> = ({
+  as: Component,
+  animation,
+  delay,
+  duration,
+  easingFunction,
+  children,
+  ...props
+}: Props) => {
+  return (
+    <Component
+      data-aos={animation}
+      data-aos-duration={duration || 400}
+      data-aos-easing={easingFunction || "ease-in-out"}
+      data-aos-delay={delay || 0}
+      {...props}>
+      {children}
+    </Component>
+  );
+};
+
+export { Animate };
