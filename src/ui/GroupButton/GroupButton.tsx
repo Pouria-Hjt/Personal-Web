ba53070470e72ed56c45265fd32b3ce89d7985dc
pouria-hjt <poriw.hjt@gmail.com>
2024-09-10 21:08:52 +0100

    ref(groupBtn): fix group hover

--- src/ui/GroupButton/GroupButton.tsx
+++ src/ui/GroupButton/GroupButton.tsx
@@ -35,7 +35,7 @@ const GroupButton = ({
 }: Props) => {
   const Comp = as === "button" ? "button" : (Link as React.ElementType);
   return (
-    <Comp className={cn("h-12 w-fit flex group", className)} {...props}>
+    <Comp className={cn("h-12 w-fit flex group/btn", className)} {...props}>
       <span
         className={cn(
           "bg-primary capitalize h-full flex justify-center items-center px-6 rounded-full font-medium",
@@ -48,10 +48,10 @@ const GroupButton = ({
           "bg-primary rounded-full h-full aspect-square  relative overflow-hidden",
           classNames?.iconButton
         )}>
-        <div className="size-full flex justify-center items-center absolute inset-0 group-hover:-top-1/2 group-hover:left-1/2 group-hover:translate-x-10 group-hover:-translate-y-10 transition-all duration-700">
+        <div className="size-full flex justify-center items-center absolute inset-0 group-hover/btn:-top-1/2 group-hover/btn:left-1/2 group-hover/btn:translate-x-10 group-hover/btn:-translate-y-10 transition-all duration-700">
           {icon ? icon : <MoveUpRight />}
         </div>
-        <div className="size-full flex justify-center items-center absolute top-1/2 -left-1/2 -translate-x-10 translate-y-10 group-hover:inset-0 group-hover:translate-y-0 group-hover:translate-x-0 transition-all duration-700">
+        <div className="size-full flex justify-center items-center absolute top-1/2 -left-1/2 -translate-x-10 translate-y-10 group-hover/btn:inset-0 group-hover/btn:translate-y-0 group-hover/btn:translate-x-0 transition-all duration-700">
           {icon ? icon : <MoveUpRight />}
         </div>
       </span>
