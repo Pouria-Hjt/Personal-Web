7a28c2ca3515891752fdd2320fc448436d8c8ec4
pouria-hjt <poriw.hjt@gmail.com>
2024-11-30 19:22:58 +0000

    ref(UI) add before for layout

--- src/app/(landings)/layout.tsx
+++ src/app/(landings)/layout.tsx
@@ -7,7 +7,7 @@ export default function LandingsLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex h-fit min-h-screen flex-col bg-[#0A090D]">
+    <div className="relative flex h-fit min-h-screen flex-col bg-[#0A090D] before:absolute before:left-0 before:top-0 before:z-50 before:size-[500px] before:-translate-x-[250px] before:-translate-y-[250px] before:rounded-full before:bg-[#FF6900]/20 before:blur-[200px]">
       <BackgroundBeamsWithCollision className="flex-col items-start justify-start">
         <Navbar />
         {children}
